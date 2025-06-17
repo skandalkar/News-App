30934c498e66fd5bcbde7287d53f78e2c96e469e
Santosh Kandalkar <skandalkar1508@gmail.com>
2025-06-17 16:27:23 +0530

    Project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center mt-10">
+        Hello world!
+        This is Smart News App with news aggregation and summarization and facts validation using AI.
+      </h1>
+    </>
+  );
+}
+
+export default App;
