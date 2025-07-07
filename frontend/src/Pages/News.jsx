127ae551d374b82ffa4ea72010a81108d00cd373
Santosh Kandalkar <skandalkar1508@gmail.com>
2025-07-07 19:33:54 +0530

    UI issues solved

--- frontend/src/Pages/News.jsx
+++ frontend/src/Pages/News.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import NewsContainer from "../components/NewsContainer";
 import Navbar from "../components/Navbar";
 import { useParams } from "react-router-dom";
+import { Loader2 } from 'lucide-react';
 
 function News() {
 
@@ -13,12 +14,14 @@ function News() {
 
   const fetchNews = async () => {
     try {
+      setLoading(true);
+      // Fetch news articles from the backend API based on the category
       const response = await axios.get(`${backEndUrl}/api/news`, {
         params: { category },
       });
       setArticles(response.data.articles);
       setLoading(false);
-    } 
+    }
     catch (error) {
       console.error("Error fetching news:", error);
       setLoading(false);
@@ -29,27 +32,34 @@ function News() {
     fetchNews();
   }, [category, backEndUrl]);
 
+
   return (
     <div>
       <Navbar />
 
       <div className="h-screen text-black bg-white py-24 px-4 md:px-0">
-        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2  lg:grid-cols-4 gap-7">
+        {
+          loading ? (
+            <div className="h-full flex items-center justify-center w-full">
+              <div className='flex flex-col items-center justify-center'>
+                <Loader2 className='h-12 w-12 animate-spin' />
+                <h1 className='text-gray-800 text-xl font-semibold'>Loading...</h1>
+              </div>
+            </div>
+          ) : (
 
-          {
-            articles.map((article, index) => {
-              return (
-                <NewsContainer
-                  key={index}
-                  article={article}
-                />
-              )
-            })
-          }
+            <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2  lg:grid-cols-4 gap-7">
+              {articles.map((article, index) => (
+                <NewsContainer key={index} article={article} />
+              ))}
+            </div>
 
-        </div>
+          )
+        }
+
       </div>
     </div>
+
   );
 }
 
