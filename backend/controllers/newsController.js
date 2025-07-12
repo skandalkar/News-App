d455560f2073126db630d9a75a0c564cc64fc5ec
Santosh Kandalkar <skandalkar1508@gmail.com>
2025-07-12 14:17:56 +0530

    Server error solved

--- backend/controllers/newsController.js
+++ backend/controllers/newsController.js
@@ -4,14 +4,13 @@ const axios = require('axios');
 const getNews = async (req, res) => {
     try {
         const { category = 'general' } = req.query;
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?`, {
+        const response = await axios.get(`https://gnews.io/api/v4/top-headlines?`, {
             params: {
-                country: 'us',
-                category,
-                pageSize: 20,
-            },
-            headers: {
-                'X-Api-Key': process.env.NEWS_API_KEY,
+                topic: category,       
+                lang: 'en',
+                country: 'in',
+                max: 10,              
+                token: process.env.GNEWS_API_KEY, 
             },
         });
         res.json(response.data);
@@ -30,12 +29,13 @@ const searchNews = async (req, res) => {
         return res.status(400).json({ error: "Missing search query" });
     }
     try {
-        const response = await axios.get('https://newsapi.org/v2/everything', {
+        const response = await axios.get(' https://gnews.io/api/v4/search', {
             params: {
-                q,
-                apiKey: process.env.NEWS_API_KEY,
-                pageSize: 20,
-                language: 'en',
+                q: query,
+                lang: 'en',
+                country: 'in',
+                max: 10,
+                token: process.env.GNEWS_API_KEY, // This is required
             },
         });
         res.json({ articles: response.data.articles });
